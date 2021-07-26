4480e89d328bf35c0ce869bcbbb9ff4aa5edcda2
karanikathungu-crypto <kathungudev@gmail.com>
2021-07-26 16:23:06 +0300

    Add toggle method to the user interface logic for what we do section

--- Javs/main.js
+++ Javs/main.js
@@ -1,5 +1,15 @@
+//business logic
+//api for getting emails and displaying text
+
+
+
+
 $(document).ready(function(){
+  $(".imageText").click(function(){
+    $(".details").toggle();
+
+  });
 
 
-  
+
 })
